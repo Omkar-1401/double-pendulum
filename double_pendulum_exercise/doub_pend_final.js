180843ba522bcc066151ce151a626c01a2ff85e5
Omkar-1401 <omkarnm1401@gmail.com>
2021-12-07 18:00:44 +0530

    Some changes

--- double_pendulum_exercise/doub_pend_final.js
+++ double_pendulum_exercise/doub_pend_final.js
@@ -4,6 +4,8 @@ function setup() {
     
     pg = createGraphics(1000, 1000);
     pg.background(220);
+    pg.translate(width / 4, height / 4);
+  
 }
 
 var m1 = 30;
@@ -27,10 +29,6 @@ var t = 0;
 var w1 = 0;
 var w2 = 0;
 
-
-var points = [];
-var phase1 = [];
-var phase2 = [];
 
 function ForwardEuler(f, T0, dt, t0) {
   
@@ -60,6 +58,10 @@ function draw() {
   background(220);
   image(pg, 0, 0);
   
+  pre_vect = createVector(x2, y2);
+  pre_vect1 = createVector(t1, w1);
+  pre_vect2 = createVector(t2, w2);
+  
   x1 = l1 * sin(t1);
   y1 = l1 * cos(t1);
 
@@ -87,27 +89,19 @@ function draw() {
   t2 = soln[2];
   w2 = soln[3];
   
-  vect = createVector(x2 + width / 4, y2 + height / 4);
-  points.push(vect);
+  vect = createVector(x2, y2);
   
   strokeWeight(1);
   stroke('black');
-  if (points.length > 1) {
-    let v1 = points[points.length - 1];
-    let v2 = points[points.length - 2];
-    pg.line(v1.x, v1.y, v2.x, v2.y);
+  if (pre_vect.x != 0) {
+    pg.line(pre_vect.x, pre_vect.y, vect.x, vect.y);  
   }
   
 
   vect1 = createVector(t1, w1);
   fill('red');
 
-  phase1.push(vect1);
-  if (phase1.length > 1) {
-    let v3 = phase1[phase1.length - 1];
-    let v4 = phase1[phase1.length - 2];
-    pg.line(v3.x * 30 + 400 + width / 4, v3.y * 250 + height / 4, v4.x * 30 + 400 + width / 4, v4.y * 250 + height / 4);
-  }
+  pg.line(pre_vect1.x * 30 + 400, pre_vect1.y * 250, vect1.x * 30 + 400, vect1.y * 250);
 
   circle(vect1.x * 30 + 400, vect1.y * 250, 7);
   
@@ -121,22 +115,16 @@ function draw() {
   text('big bob', 360, 220);
   
   vect2 = createVector(t2, w2);
-
-  phase2.push(vect2);
   
   stroke('black');
-  if (phase2.length > 1) {
-    let v5 = phase2[phase2.length - 1];
-    let v6 = phase2[phase2.length - 2];
-    pg.line(v5.x * 5 + 200 + width / 4, v5.y * 100 + 400 + height / 4, v6.x * 5 + 200 + width / 4, v6.y * 100 + 400 + height / 4);
-  }
-
+  pg.line(pre_vect2.x * 5 + 200, pre_vect2.y * 100 + 400, vect2.x * 5 + 200, vect2.y * 100 + 400);
   
   fill('red');
   circle(vect2.x * 5 + 200, vect2.y * 100 + 400, 7);
   
   fill('gray');
   stroke('gray');
-  text('small bob', 100, 600);
-  
+  text('small bob', 100, 600); 
+
+
 }
